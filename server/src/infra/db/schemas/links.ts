0a0282bddb52a675bd841de079bc3b7eb7ab35fc
Jean Nunes <jean.nunes@uzzipay.com>
2025-09-04 23:26:31 -0400

    Set up database by Drizzle

---
+++ server/src/infra/db/schemas/links.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { uuidv7 } from "uuidv7";
+
+export const links = pgTable("links", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  originalUrl: text("original_url").notNull(),
+  shortUrl: text("short_url").notNull().unique(),
+  views: integer().notNull().default(0),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
