c0ecf1503ed2103b5bf1d362cce10ecde49e74e8
Jean Nunes <jean.nunes@uzzipay.com>
2025-09-14 16:16:53 -0400

    Create button component

---
+++ web/src/components/ui/button.tsx
@@ -0,0 +1,51 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-3 whitespace-nowrap rounded-md transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        primary:
+          "bg-blue-base text-md text-white rounded-lg hover:bg-blue-dark cursor-pointer",
+        secondary:
+          "bg-gray-200 text-sm text-gray-500 rounded-sm hover:border border-blue-base cursor-pointer",
+      },
+      size: {
+        default: "h-12 px-5 gap-3 w-full",
+        sm: "h-8 gap-1.5 px-2",
+        icon: "h-8 w-8 gap-2 p-0",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
+    },
+  }
+);
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
