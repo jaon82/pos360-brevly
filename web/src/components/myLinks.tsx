2f3fbba3c40e0b2659bfa50d560347ff791207cf
Jean Nunes <jean.nunes@uzzipay.com>
2025-09-15 23:53:33 -0400

    Add MyLlinks component

---
+++ web/src/components/myLinks.tsx
@@ -0,0 +1,29 @@
+import { DownloadSimpleIcon, LinkIcon } from "@phosphor-icons/react";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Separator } from "./ui/separator";
+
+export default function MyLinks() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex justify-between items-center">
+          <span>Meus links</span>
+          <Button variant="secondary" size="sm" disabled>
+            <DownloadSimpleIcon />
+            Baixar CSV
+          </Button>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Separator className="mb-4" />
+        <div className="flex flex-col gap-3 items-center pb-6 pt-4">
+          <LinkIcon className="text-[2rem] text-gray-400" />
+          <div className="text-xs text-gray-500 uppercase">
+            ainda não existem links cadastrados
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,13 +1,15 @@
 import logo from "@/assets/logo.svg";
 import Form from "@/components/form";
+import MyLinks from "@/components/myLinks";
 
 export default function Home() {
   return (
-    <div className="border max-w-screen-xl mx-auto flex flex-col">
+    <div className="max-w-screen-xl mx-auto flex flex-col gap-3 px-3">
       <div className="flex justify-center items-center mt-8 mb-3">
         <img src={logo} className="h-8" />
       </div>
       <Form />
+      <MyLinks />
     </div>
   );
 }
